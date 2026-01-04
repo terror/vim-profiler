53ce4e8308d7fa4d74352b2a659e85581679d111
Liam <liam@scalzulli.com>
2026-01-04 00:03:58 -0500

    Lint

--- src/plugin.rs
+++ src/plugin.rs
@@ -41,7 +41,7 @@ impl Plugin {
     values.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
     let mid = values.len() / 2;
-    if values.len() % 2 == 0 {
+    if values.len().is_multiple_of(2) {
       (values[mid - 1] + values[mid]) / 2.0
     } else {
       values[mid]
